202f92ce97334112a0ef49676769ca5d659d7e27
Kurcha <theyhyrik@gmail.com>
2023-11-30 07:02:44 +0300

    Now uploads all sheets

--- WebApplication1/Domain/Services/ExcelParsingService.cs
+++ WebApplication1/Domain/Services/ExcelParsingService.cs
@@ -36,61 +36,63 @@ namespace WebWeatherApi.Domain.Services
             using (var stream = file.OpenReadStream())
             {
                 IWorkbook workbook = new XSSFWorkbook(stream);
-                ISheet sheet = workbook.GetSheetAt(0);
-
-
-                for (int i = 4; i <= sheet.LastRowNum; i++)
+                for (int sheetIndex = 0; sheetIndex < workbook.NumberOfSheets; sheetIndex++)
                 {
-                    IRow row = sheet.GetRow(i);
-                    if (row != null)
+                    ISheet sheet = workbook.GetSheetAt(sheetIndex);
+                    for (int i = 4; i <= sheet.LastRowNum; i++)
                     {
-                        //form date
-                        string? date = getCellValue(row, 0);
-                        string? time = getCellValue(row, 1);
-
-                        string filename = file.FileName;
-
-                        if (date == null || time == null)
-                            InvalidExcelFormatException.ThrowInvalidDateFormatException(date, time, filename);
-
-                        string? temperature = getCellValue(row, 2);
-
-                        if (temperature == null)
-                            throw new InvalidExcelFormatException("File + " + filename + "can't be parsed." + "Temperature for the record should be specified");
-
-                        DateTimeOffset? dateTime = parseDateTime(date!, time!);
-                        if (dateTime == null)
-                            InvalidExcelFormatException.ThrowInvalidDateFormatException(date, time, filename);
-
-
-                        string? humidity = getCellValue(row, 3);
-                        string? dewPoint = getCellValue(row, 4);
-                        string? pressure = getCellValue(row, 5);
-                        string? windDirection = getCellValue(row, 6);
-                        string? windSpeed = getCellValue(row, 7);
-                        string? cloudiness = getCellValue(row, 8);
-                        string? cloudBase = getCellValue(row, 9);
-                        string? visibility = getCellValue(row, 10);
-
-                        WeatherRecord weatherRecord = WeatherRecord.parseAndCreate(0, dateTime!.Value, temperature, humidity, dewPoint, pressure, windDirection, windSpeed, cloudiness, cloudBase, visibility);
-
-                        string? weatherRecordDetailsDescription = getCellValue(row, 11);
-
-                        if (weatherRecordDetailsDescription != null)
+                        IRow row = sheet.GetRow(i);
+                        if (row != null)
                         {
-                            WeatherRecordDetails? weatherRecordDetails = _context!.WeatherRecordDetails!.FirstOrDefault(wr =>
-                               wr.Description == weatherRecordDetailsDescription
-                            );
-                            if (weatherRecordDetails == null)
-                            {
-                                weatherRecordDetails = new WeatherRecordDetails(0, weatherRecordDetailsDescription);
-                                weatherRecordDetails = _context.WeatherRecordDetails.Add(weatherRecordDetails).Entity;
-                            }
+                            //form date
+                            string? date = getCellValue(row, 0);
+                            string? time = getCellValue(row, 1);
 
-                            weatherRecord.WeatherRecordDetails = weatherRecordDetails;
+                            string filename = file.FileName;
+
+                            if (date == null || time == null)
+                                InvalidExcelFormatException.ThrowInvalidDateFormatException(date, time, filename);
+
+                            string? temperature = getCellValue(row, 2);
+
+                            if (temperature == null)
+                                throw new InvalidExcelFormatException("File + " + filename + "can't be parsed." + "Temperature for the record should be specified");
+
+                            DateTimeOffset? dateTime = parseDateTime(date!, time!);
+                            if (dateTime == null)
+                                InvalidExcelFormatException.ThrowInvalidDateFormatException(date, time, filename);
+
+
+                            string? humidity = getCellValue(row, 3);
+                            string? dewPoint = getCellValue(row, 4);
+                            string? pressure = getCellValue(row, 5);
+                            string? windDirection = getCellValue(row, 6);
+                            string? windSpeed = getCellValue(row, 7);
+                            string? cloudiness = getCellValue(row, 8);
+                            string? cloudBase = getCellValue(row, 9);
+                            string? visibility = getCellValue(row, 10);
+
+                            WeatherRecord weatherRecord = WeatherRecord.parseAndCreate(0, dateTime!.Value, temperature, humidity, dewPoint, pressure, windDirection, windSpeed, cloudiness, cloudBase, visibility);
+
+                            string? weatherRecordDetailsDescription = getCellValue(row, 11);
+
+                            if (weatherRecordDetailsDescription != null)
+                            {
+                                WeatherRecordDetails? weatherRecordDetails = _context!.WeatherRecordDetails!.FirstOrDefault(wr =>
+                                   wr.Description == weatherRecordDetailsDescription
+                                );
+                                if (weatherRecordDetails == null)
+                                {
+                                    weatherRecordDetails = new WeatherRecordDetails(0, weatherRecordDetailsDescription);
+                                    weatherRecordDetails = _context.WeatherRecordDetails.Add(weatherRecordDetails).Entity;
+                                }
+
+                                weatherRecord.WeatherRecordDetails = weatherRecordDetails;
+                            }
+                            _context.WeatherRecords.Add(weatherRecord);
+                            await _context.SaveChangesAsync();
                         }
-                        _context.WeatherRecords.Add(weatherRecord);
-                        await _context.SaveChangesAsync();
+
                     }
                 }
             }
